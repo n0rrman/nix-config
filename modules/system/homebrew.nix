95e193edeae31f6443e8092d40e9025563988ecc
n0rrman <mail@henriknorrman.com>
2025-01-21 00:50:21 +0100

    add homebrew casks

--- modules/system/homebrew.nix
+++ modules/system/homebrew.nix
@@ -3,6 +3,11 @@
     enable = true;
     casks = [
       "ghostty"
+      "messenger"
+      "discord"
+      "jagex"
+      "battle-net"
+      "curseforge"
     ];
     cleanup = "uninstall";
     caskArgs.no_quarantine = true;
