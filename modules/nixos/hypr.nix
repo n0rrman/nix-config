82adcffa497ef7daee553dc6da7cac2c48dbe26a
norrman <mail@henriknorrman.com>
2025-02-21 17:36:49 +0100

    hypr cleanup

--- modules/nixos/hypr.nix
+++ modules/nixos/hypr.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     hyprlock
     hypridle
@@ -6,27 +7,15 @@
   ];
 
   home.file.".config/hypr" = {
-    source = builtins.path { name = "hypr_config"; path = ./hypr; };
+    source = builtins.path {
+      name = "hypr_config";
+      path = ./hypr;
+    };
   };
 
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs.hyprland;
     systemd.enable = true;
-    # settings = {
-      # "$mod" = "SUPER";
-
-      # cursor = {
-        # enable_hyprcursor = false;
-      # };
-
-      # bind = [
-      # ];
-      # extraConfig = ''
-      # input * {
-      # input = {
-        # natural_scroll = true;
-      # };
-    # };
   };
 }
