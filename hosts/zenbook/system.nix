fe9ac01c00d50302fc952201ed14c669280dc318
norrman <mail@henriknorrman.com>
2025-02-27 09:09:41 +0100

    remove copy paste keys (again)

--- hosts/zenbook/system.nix
+++ hosts/zenbook/system.nix
@@ -23,12 +23,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "se";
-    variant = "mac";
-  };
-
   # time sync
   systemd.timers.ntp.enable = true;
 
