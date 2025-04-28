c61bc39b3d19740c49ed5f2bd90fd9fc3aec2ba6
norrman <mail@henriknorrman.com>
2025-04-28 09:47:52 +0200

    pin docker ips for firewall

--- modules/shared/work.nix
+++ modules/shared/work.nix
@@ -6,20 +6,23 @@
     COMPOSE_DOCKER_CLI_BUILD = "1";
     DOCKER_BUILDKIT = "1";
   };
-  virtualisation.docker.enable = true;
-  virtualisation.docker.extraOptions = "--default-ulimit nofile=1024000:1024000 --mtu=1340";
+
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--bip=\"172.26.0.1/16\" --default-address-pool=\"base=172.30.0.0/16,size=24\" --ipv6=false --default-ulimit nofile=1024000:1024000 --mtu=1340";
+  };
 
   networking.firewall = {
     trustedInterfaces = [ "docker0" ];
     checkReversePath = false;
-    # extraCommands = ''
-    #   ip46tables -A INPUT -s 172.17.0.0/16 -j ACCEPT
-    #   ip46tables -A OUTPUT -d 172.17.0.0/16 -j ACCEPT
-    # '';
-    # extraStopCommands = ''
-    #   ip46tables -D INPUT -s 172.17.0.0/16 -j ACCEPT 2>/dev/null || true
-    #   ip46tables -D OUTPUT -d 172.17.0.0/16 -j ACCEPT 2>/dev/null || true
-    # '';
+    extraCommands = ''
+      ip46tables -A INPUT -s 172.17.0.0/16 -j ACCEPT
+      ip46tables -A OUTPUT -d 172.17.0.0/16 -j ACCEPT
+    '';
+    extraStopCommands = ''
+      ip46tables -D INPUT -s 172.17.0.0/16 -j ACCEPT 2>/dev/null || true
+      ip46tables -D OUTPUT -d 172.17.0.0/16 -j ACCEPT 2>/dev/null || true
+    '';
   };
 
   environment.systemPackages = with pkgs; [
