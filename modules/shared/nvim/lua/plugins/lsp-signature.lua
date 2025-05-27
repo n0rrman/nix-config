34f088171974f2a54d5050bdb769f9fd92a1c601
norrman <mail@henriknorrman.com>
2025-05-27 11:04:24 +0200

    clean up nvim plugins

--- modules/shared/nvim/lua/plugins/lsp-signature.lua
+++ modules/shared/nvim/lua/plugins/lsp-signature.lua
@@ -1,18 +1,18 @@
 return {
-  "ray-x/lsp_signature.nvim",
-  enabled = true,
-  event = "InsertEnter",
-  opts = {
-    bind = true,
-    doc_lines = 10,
-    max_height = 12,
-    max_width = 80,
-    wrap = true,
-    handler_opts = {
-      border = "rounded"
-    },
-    floating_window = true,
-    hint_enable = true,
-    hint_prefix = "🔍 "
-  },
+	"ray-x/lsp_signature.nvim",
+	enabled = true,
+	event = "InsertEnter",
+	opts = {
+		bind = true,
+		doc_lines = 10,
+		max_height = 12,
+		max_width = 80,
+		wrap = true,
+		handler_opts = {
+			border = "rounded",
+		},
+		floating_window = true,
+		hint_enable = true,
+		hint_prefix = "🔍 ",
+	},
 }
--- modules/shared/nvim/lua/plugins/telescope.lua
+++ modules/shared/nvim/lua/plugins/telescope.lua
@@ -1,31 +1,29 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		enabled = true,
-		cmd = "Telescope",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-			"debugloop/telescope-undo.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				config = function()
-					require("telescope").load_extension("fzf")
-					require("telescope").load_extension("ui-select")
-					require("telescope").load_extension("undo")
-				end,
-			},
+	"nvim-telescope/telescope.nvim",
+	enabled = true,
+	cmd = "Telescope",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-ui-select.nvim",
+		"debugloop/telescope-undo.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+				require("telescope").load_extension("ui-select")
+				require("telescope").load_extension("undo")
+			end,
 		},
-		opts = {
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
+	},
+	opts = {
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
 			},
 		},
 	},
